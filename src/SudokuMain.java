73f1f855e02327386c1aad9383f9bc0a24031996
vpuris3826 <vpuris3826@student.pps.net>
2024-10-01 09:56:16 -0700

    figure out how to append to new array

--- src/SudokuMain.java
+++ src/SudokuMain.java
@@ -6,24 +6,35 @@ public class SudokuMain {
         new SudokuMain();
     }
 
+    int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    public SudokuMain() {
+        makeBoxOneOne(arr);
+    }
+
     //int difficulty; // if difficulty = 1, blank boxes = ...
 
-    public void makeBoxOneOne()
-    {
+    public void makeBoxOneOne(int [] array) {
         System.out.println("check");
-        int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int j = (int) (Math.random() * (arr.length + 1));
-        int newArr[] = new int[arr.length - 1];
-        //int appArray[] = {};
-        for(int i = 0, k = 0; i < arr.length; i++){
-            if(i!=j){
+        int arr[] = array;
+        int[] newArr = null;
+        int[] appArr = new int[8];
+        int j = (int) (Math.random() * (arr.length));
+        newArr = new int[arr.length - 1];
+        int appArray[] = {};
+        for (int i = 0, k = 0; i < arr.length; i++) {
+            if (i != j) {
                 newArr[k] = arr[i];
                 k++;
             }
         }
         System.out.println("Before: " + Arrays.toString(arr));
         System.out.println("After: " + Arrays.toString(newArr));
-        System.out.println("New array: ");
+        //System.out.println("Before: " + Arrays.toString(arr));
+        //System.out.println("After: " + Arrays.toString(newArr));
+        //System.out.println("New array: ");
+        //return arr;
+        //return newArr;
     }
 
 }
