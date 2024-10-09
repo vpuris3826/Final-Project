12a2b133a8633144fb9d1a1979a4acd8438eb112
vpuris3826 <vpuris3826@student.pps.net>
2024-10-09 09:57:39 -0700

    figure out how to save appended array

--- src/SudokuMain.java
+++ src/SudokuMain.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 //import java.text.*;
 
 public class SudokuMain {
@@ -6,10 +8,18 @@ public class SudokuMain {
         new SudokuMain();
     }
 
+    //arrays
     int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int newArr[] = new int[]{};
+    Integer appArr[] = {};
 
     public SudokuMain() {
         makeBoxOneOne(arr);
+        int r = 2;
+        while(r <= 9){
+            makeBoxOneOne(newArr);
+            r++;
+        }
     }
 
     //int difficulty; // if difficulty = 1, blank boxes = ...
@@ -17,24 +27,31 @@ public class SudokuMain {
     public void makeBoxOneOne(int [] array) {
         System.out.println("check");
         int arr[] = array;
-        int[] newArr = null;
-        int[] appArr = new int[8];
+        //int[] newArr = null;
         int j = (int) (Math.random() * (arr.length));
         newArr = new int[arr.length - 1];
-        int appArray[] = {};
+        int n = 9;
         for (int i = 0, k = 0; i < arr.length; i++) {
             if (i != j) {
                 newArr[k] = arr[i];
                 k++;
             }
         }
+        int appadd = arr[j];
+        appArr = addX(n, appArr, appadd);
+        System.out.println("addX result: " + Arrays.toString(appArr));
         System.out.println("Before: " + Arrays.toString(arr));
-        System.out.println("After: " + Arrays.toString(newArr));
-        //System.out.println("Before: " + Arrays.toString(arr));
-        //System.out.println("After: " + Arrays.toString(newArr));
-        //System.out.println("New array: ");
-        //return arr;
-        //return newArr;
+        System.out.println("After (short array): " + Arrays.toString(newArr));
+    }
+
+    public Integer[] addX(int n, Integer appArr[], int x){
+        int i;
+        List<Integer>arrlist
+                = new ArrayList<Integer>(
+                        Arrays.asList(appArr));
+        arrlist.add(x);
+        appArr = arrlist.toArray(appArr);
+        return appArr;
     }
 
 }
