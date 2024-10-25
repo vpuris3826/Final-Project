775f30b1128468852f3d29cb47b986e98d0c0d59
vpuris3826 <vpuris3826@student.pps.net>
2024-10-25 09:47:24 -0700

    figure out

--- src/SudokuMain.java
+++ src/SudokuMain.java
@@ -9,15 +9,40 @@ public class SudokuMain {
     }
 
     //arrays
-    int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int newArr[] = new int[]{};
-    Integer appArr[] = {};
+    int arr1[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr2[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr3[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr4[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr5[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr6[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr7[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr8[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr9[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int newArr1[] = new int[]{};
+    int newArr2[] = new int[]{};
+    int newArr3[] = new int[]{};
+    int newArr4[] = new int[]{};
+    int newArr5[] = new int[]{};
+    int newArr6[] = new int[]{};
+    int newArr7[] = new int[]{};
+    int newArr8[] = new int[]{};
+    int newArr9[] = new int[]{};
+    Integer appArr1[] = {};
     Integer appArr2[] = {};
+    Integer appArr3[] = {};
+    Integer appArr4[] = {};
+    Integer appArr5[] = {};
+    Integer appArr6[] = {};
+    Integer appArr7[] = {};
+    Integer appArr8[] = {};
+    Integer appArr9[] = {};
 
     public SudokuMain() {
         //saveApp();
-        //saveApp2();
-        saver();
+        //saveApp(appArr);
+        //saver();
+        //makeLine(arr);
+        printer(appArr1);
     }
 
     //int difficulty; // if difficulty = 1, blank boxes = ...
@@ -32,27 +57,7 @@ public class SudokuMain {
         return appArr;
     }
 
-    public Integer[] makeLine(int [] array) {
-        System.out.println("check");
-        int arr[] = array;
-        int j = (int) (Math.random() * (arr.length));
-        newArr = new int[arr.length - 1];
-        int n = 9;
-        for (int i = 0, k = 0; i < arr.length; i++) {
-            if (i != j) {
-                newArr[k] = arr[i];
-                k++;
-            }
-        }
-        int appadd = arr[j];
-        Integer[] append = addX(n, appArr, appadd);
-        System.out.println("addX result: " + Arrays.toString(appArr));
-        System.out.println("Before: " + Arrays.toString(arr));
-        System.out.println("After (short array): " + Arrays.toString(newArr));
-        return append;
-    }
-
-    /*public Integer[] makeLine2(int [] array) {
+    public Integer[] makeLine(int [] array, int [] newArr) {
         System.out.println("check 2");
         int arr[] = array;
         int j = (int) (Math.random() * (arr.length));
@@ -65,39 +70,27 @@ public class SudokuMain {
             }
         }
         int appadd = arr[j];
-        appArr2 = addX(n, appArr2, appadd);
-        System.out.println("addX result: " + Arrays.toString(appArr2));
+        appArr1 = addX(n, appArr1, appadd);
+        System.out.println("addX result: " + Arrays.toString(appArr1));
         System.out.println("Before: " + Arrays.toString(arr));
         System.out.println("After (short array): " + Arrays.toString(newArr));
-        return appArr2;
-    }*/
-
-    public Integer [] saveApp(){
-        makeLine(arr);
-        int r = 2;
-        while(r <= 8){
-            makeLine(newArr);
-            r++;
-        }
-        makeLine(newArr);
-        return appArr;
+        return appArr1;
     }
 
-    public Integer[] saver(){
-        saveApp();
-        System.out.println(appArr);
-        return appArr;
-    }
-
-    /*public Integer[] saveApp2(Integer appArr2[]){
-        makeLine2(arr);
+    public void saveApp(int [] array, int [] newArr, Integer appArr[]){
+        makeLine(array, newArr);
         int r = 2;
         while(r <= 9){
-            makeLine2(newArr);
+            makeLine(newArr, newArr);
             r++;
         }
-        System.out.println(appArr2);
-        return appArr2;
-    }*/
+    }
+
+    public void printer(Integer appArr[]){
+        saveApp(arr1, newArr1, appArr1);
+        System.out.println(Arrays.toString(appArr1));
+        saveApp(arr2, newArr2, appArr2);
+        System.out.println(Arrays.toString(appArr2));
+    }
 
 }
