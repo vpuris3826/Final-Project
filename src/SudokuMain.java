8c89dc592915d049ced0cdfeda2db97709b161f8
vpuris3826 <vpuris3826@student.pps.net>
2024-10-31 09:47:25 -0700

    some loop isn't ending, fix

--- src/SudokuMain.java
+++ src/SudokuMain.java
@@ -7,49 +7,81 @@ public class SudokuMain {
         new SudokuMain();
     }
 
-    //arrays
-    //int[] base = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    //Integer app[];
-    //Integer[] arr;
-
     public SudokuMain() {
-        //removal();
-        System.out.print(Arrays.toString(cell1x1));
+        removal();
+        cellMaker(cell1x1);
     }
 
-    //int difficulty; // if difficulty = 1, blank boxes = ...
-
-    /*int [][] cellBase =
+    int [][] base =
             { {1, 2, 3},
             {4, 5, 6},
-            {7, 8, 9}};*/
+            {7, 8, 9}};
     int [][] cell1x1 = new int[3][3];
     int [][] cell2x2 = new int[3][3];
     int [][] cell3x3 = new int[3][3];
 
     public int[][] removal(){
-        int [][] cellBase =
-                { {1, 2, 3},
-                        {4, 5, 6},
-                        {7, 8, 9}};
-        int [][] oneToNineMinus = new int[cellBase.length - 1][cellBase.length - 1];
-        int j = (int)(Math.random() * 9);
-        for(int r = 0, k = 0; r < cellBase.length; r++){
-            for
-            if(i != j){
-                oneToNineMinus[k] = cellBase[i];
-                k++;
+        int [][] cellBase = base;
+        int [][] oneToNineMinus = new int [0][0];
+        for(int n = 0; n < 3; n++) {
+            oneToNineMinus = new int[cellBase.length - 1][cellBase.length - 1];
+            int j = (int) (Math.random() * 3);
+            for (int r = 0, k = 0; r < cellBase.length; r++) {
+                if(r != j) {
+                    int m = (int) (Math.random() * 3);
+                    for (int c = 0, l = 0; c < cellBase.length; c++) {
+                        if (c != m) {
+                            oneToNineMinus[k][l] = cellBase[r][c];
+                            l++;
+                        }
+                    }
+                    k++;
+                }
             }
         }
-        System.out.println("before: " + Arrays.toString(cellBase));
-        System.out.println("after: " + Arrays.toString(oneToNineMinus));
+        System.out.println("before: ");
+        printer(cellBase);
+        System.out.println("after: ");
+        printer(oneToNineMinus);
         return oneToNineMinus;
     }
 
-    /*public int[][] adding(){
-        for(int n = 1; n <= 9; n++){
-
+    public void printer(int [][] cellX){
+        for(int r = 0; r < cellX.length; r++){
+            for(int c = 0; c < cellX[r].length; c++){
+                System.out.print(cellX [r][c] + " ");
+            }
+            System.out.println();
         }
-    }*/
+    }
+
+    public int cellSum(int [][] cellX){
+        int sum = 0;
+        for(int r = 0; r < 3; r++){
+            for(int c = 0; c < 3; c++){
+                sum += cellX[r][c];
+            }
+        }
+        return sum;
+    }
+
+    public int [][] appendCell(int row, int col, int num, int [][] cellZ){
+        cellZ[row][col] = num;
+        return cellZ;
+    }
+
+    public void cellMaker(int [][] cellY){
+        int sum = cellSum(cellY);
+        while(sum != 45) {
+            for (int n = 1; n <= 9; n++) {
+                int r = (int) (Math.random() * 3);
+                int c = (int) (Math.random() * 3);
+                if (cellY[r][c] == 0) {
+                    appendCell(r, c, n, cellY);
+                }
+            }
+        }
+        //return CellX
+    }
 
 }
