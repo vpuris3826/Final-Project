e5f2404d784439c13f09e2c710b1f47b0c4877ec
vpuris3826 <vpuris3826@student.pps.net>
2024-11-08 09:58:07 -0800

    some loop isn't ending, fix

--- src/SudokuMain.java
+++ src/SudokuMain.java
@@ -1,17 +1,22 @@
 //import java.text.*;
 
-import java.util.Arrays;
-
 public class SudokuMain {
     public static void main(String[] args) {
         new SudokuMain();
     }
 
     public SudokuMain() {
+        System.out.println("Cell 1x1");
         cellMaker(cell1x1);
+        System.out.println("Cell 2x2");
+        cellMaker(cell2x2);
+        System.out.println("Cell 3x3");
+        cellMaker(cell3x3);
+        System.out.println("base test");
+        basemaker(base, cell1x1, cell2x2, cell3x3);
     }
 
-    int [][] base = new int[3][3];
+    int [][] base = new int[9][9];
     int [][] cell1x1 = new int[3][3];
     int [][] cell2x2 = new int[3][3];
     int [][] cell3x3 = new int[3][3];
@@ -40,10 +45,6 @@ public class SudokuMain {
         return cellZ;
     }
 
-    public int [][] appendBase(int row, int col, int [][]cellA, int [][] base){
-        base
-    }
-
     public int [][] cellMaker(int [][] cellY){
         int sum = cellSum(cellY);
         int n = 1;
@@ -60,8 +61,39 @@ public class SudokuMain {
         return cellY;
     }
 
-    public int [][] basemaker(int [][] base){
-
+    public int [][] basemaker(int [][] base, int [][] cell1x1, int [][] cell2x2, int [][] cell3x3){
+        for(int r = 0; r < 3; r++){
+            for(int c = 0; c < 3; c++){
+                int n = cell1x1[r][c];
+                appendCell(r, c, n, base);
+            }
+        }
+        for(int rc = 0; rc < 3; rc++){
+            int r = 3;
+            int c = 3;
+            for(int cc = 0; cc < 3; cc++){
+                int n = cell2x2[rc][cc];
+                appendCell(r, c, n, base);
+                System.out.println("rc: " + rc + "cc: " + cc);
+            }
+            r++;
+            c++;
+        }
+        for(int r = 6; r < 9; r++){
+            for(int c = 6; c < 9; c++){
+                if(base[r][c] == 0){
+                    for(int rc = 0; rc < 3; rc++){
+                        for(int cc = 0; cc < 3; cc++){
+                            if(base[r][c] == 0){
+                                int n = cell3x3[rc][cc];
+                                appendCell(r, c, n, base);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        printer(base);
         return base;
     }
 
